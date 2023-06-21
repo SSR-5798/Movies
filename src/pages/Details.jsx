7aa54aa0d094dc762275dc7ea6a8970d739bbfe8
Shubham <shubhamraikar5798@gmail.com>
2023-06-21 16:48:10 +0530

    Modified some changes in Details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -14,7 +14,6 @@ const { data: videos } = useFetch(`${mediaType}/${id}/videos`) // video section
 const { data: similarVideos, laoding:similarVideosLoading } = useFetch(`${mediaType}/${id}/similar`) // similar videos
 const { data: recommendVideos, laoding:recommendVideosLoading } = useFetch(`${mediaType}/${id}/recommendations`) // recommended videos
 
-
   return (
     <>
       <DetailBanner data={data} loading={loading} mediaType={mediaType} crew={credits?.crew} id={videos?.results[0]?.key}/>
